8288787bb30f2b83752e179ada03a664b2b6d132
Shalu Dhundele <shalu.dhundele@technoduxx.com>
2025-12-18 18:15:26 +0530

    Second commit

--- src/Session7/Lab004.java
+++ src/Session7/Lab004.java
@@ -2,6 +2,12 @@ package Session7;
 
 public class Lab004 {
     public static void main(String[] args) {
-
+        // Swap Program
+        int a= 100;
+        int b= 50;
+        a=a-b;
+        b=a+a;
+        System.out.println(a);
+        System.out.println(b);
     }
 }

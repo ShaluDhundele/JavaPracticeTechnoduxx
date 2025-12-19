81bc42dad0b96a4ead0dfc5027ce6d6d8f3d7ba1
Shalu Dhundele <shalu.dhundele@technoduxx.com>
2025-12-19 17:31:46 +0530

    Third Commit

---
+++ src/Session8/Lab008.java
@@ -0,0 +1,19 @@
+package Session8;
+
+import java.util.Scanner;
+
+public class Lab008 {
+    public static void main (String[] args){
+        Scanner user = new Scanner (System.in);
+                System.out.println("Enter the year:");
+                int year = user.nextInt();
+
+            if (year%4==0 && year%100!=0 && year%400==0) {
+                System.out.println(year+ "is the leap year");
+            }
+            else {
+                System.out.println(year + "is not the leap year");
+            }
+
+            }
+    }

6a058d09c1a76700b7ef66f1810f443e3e68250c
Shalu Dhundele <shalu.dhundele@technoduxx.com>
2026-01-05 17:38:12 +0530

    Seventh Commit

---
+++ src/Session10/Lab013_Loops.java
@@ -0,0 +1,11 @@
+package Session10;
+//Print table with loop
+public class Lab013_Loops {
+    public static void main (String[] args){
+        int number = 9;
+        for (int i=1;i<=10;i++){
+            int a = number * i;
+            System.out.println(a);
+        }
+    }
+}
